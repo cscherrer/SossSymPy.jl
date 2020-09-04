50048f278566ea96822b8d16843a15f46da89a00
Documenter.jl <documenter@juliadocs.github.io>
2020-09-04 23:14:24 +0000

    build based on 703e7de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SossSymPy","category":"page"},{"location":"#SossSymPy","page":"Home","title":"SossSymPy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"SossSymPy adds SymPy integration to the Soss package.","category":"page"},{"location":"","page":"Home","title":"Home","text":"The source code is available in the GitHub repository.","category":"page"}]
+}
